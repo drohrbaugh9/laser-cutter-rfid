169316166d74e6b4b9acad46803b9db997c35862
drohrbaugh9 <david.rohrbaugh7@gmail.com>
2023-11-09 00:56:17 -0700

    improvements, new 'cursor', lcd 'emulator' for linux terminal

--- keyboard_lcd_test.py
+++ keyboard_lcd_test.py
@@ -1,24 +1,23 @@
 import keyboard
 import time
-import RPi_I2C_driver as lcd_driver
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ']
 
 def my_callback(event):
   global name, current_letter
   
   if event.name == 'up':
     current_letter -= 1
-    if current_letter < ord('A'):
-      current_letter = ord('Z')
-    #print(chr(current_letter), end=' ')
+    if current_letter < 0:
+      current_letter = len(letters) - 1
   
   if event.name == 'down':
     current_letter += 1
-    if current_letter > ord('Z'):
-      current_letter = ord('A')
-    #print(chr(current_letter), end=' ')
+    if current_letter >= len(letters):
+      current_letter = 0
   
   if event.name == 'enter':
-    name += chr(current_letter)
+    name += letters[current_letter]
   
   if event.name == 'esc':
     print(name)
@@ -27,9 +26,10 @@ def main():
   global name, current_letter
   
   name = ""
-  current_letter = 65
+  current_letter = 0
   
-  lcd = my_lcd()
+  #lcd = my_lcd()
+  lcd = lcd_linux_terminal_emulator()
   lcd.setup()
 
   keyboard.on_press(my_callback)
@@ -38,13 +38,18 @@ def main():
 
   while(True):
     name_to_display = name
-    if display_current: name_to_display += chr(current_letter)
-    display_current = not display_current
+    if display_current: name_to_display += letters[current_letter]
+    #display_current = not display_current
     
     lcd.display_string(name_to_display, 1)
-    #lcd.display_string((len(name) * ' ') + '^', 2)
+    cursor_letter = letters[current_letter]
+    if cursor_letter == ' ': cursor_letter = '_'
+    lcd.display_string((len(name) * ' ') + cursor_letter, 2)
     
     time.sleep(0.25)
+
+'''
+import RPi_I2C_driver as lcd_driver
 
 class my_lcd(lcd_driver.lcd):
   
@@ -55,6 +60,20 @@ class my_lcd(lcd_driver.lcd):
   def display_string(self, short_str, row):
     padded_str = short_str + (' ' * (16 - len(short_str)))
     self.lcd_display_string(padded_str, row)
+'''
+
+class lcd_linux_terminal_emulator:
+  
+  def setup(self):
+    print("\x1b[0;0H\
+ ________________  \n\
+|                | \n\
+|                | \n\
+ ````````````````  ", flush=True)
+  
+  def display_string(self, short_str, row):
+    padded_str = short_str + (' ' * (16 - len(short_str)))
+    print("\x1b[%s;2H%s" % (row + 1, padded_str), flush=True)
 
 if __name__ == "__main__":
   main()
