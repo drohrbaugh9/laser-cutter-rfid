f95558cd618d1db7b34a3ac2a9e94dbaf71b9f4f
drohrbaugh9 <david.rohrbaugh7@gmail.com>
2023-11-20 15:30:16 -0700

    lcd enhancements, laser_access_control reorganization

--- improved_lcd.py
+++ improved_lcd.py
@@ -4,27 +4,34 @@ import time
 
 class lcd(lcd_driver.lcd):
   
+  def __init__(self):
+    self.setup()
+  
   def setup(self):
     self.lcd_clear()
     self.backlight(1)
   
-  def display_string(self, string, row):
+  def display_string(self, string, row, display_last_16 = True, align_left = True):
     formatted_str = string
     if len(string) < 16:
-      formatted_str = string + (' ' * (16 - len(string)))
-    elif len(string) > 16:
-      formatted_str = string[-16:]
+      padding = (' ' * (16 - len(string)))
+      if align_left:
+        formatted_str = string + padding
+      else:
+        formatted_str = padding + string
+    elif len(string) > 16 and display_last_16:
+        formatted_str = string[-16:]
     self.lcd_display_string(formatted_str, row)
   
-  def display_strings(self, string_with_newlines):
+  def display_strings(self, string_with_newlines, sleep_time = 2, display_last_16 = True, align_left = True):
     list_of_strings = string_with_newlines.split('\n')
-    self.display_list_of_strings(list_of_strings)
+    self.display_list_of_strings(list_of_strings, sleep_time, display_last_16, align_left)
   
-  def display_list_of_strings(self, strings):
+  def display_list_of_strings(self, strings, sleep_time = 2, display_last_16 = True, align_left = True):
     for i in range(1, len(strings)):
-      self.display_string(strings[i -1], 1)
-      self.display_string(strings[i], 2)
-      time.sleep(2)
+      self.display_string(strings[i -1], 1, display_last_16, align_left)
+      self.display_string(strings[i], 2, display_last_16, align_left)
+      time.sleep(sleep_time)
   
   def display_uid_not_recognized(self, clear = True, row = 2):
     if row == 2:
--- laser_access_control.py
+++ laser_access_control.py
@@ -1,89 +1,41 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522 # /usr/local/lib/python3.9/dist-packages/mfrc522
+from mfrc522 import SimpleMFRC522 # https://pypi.org/project/mfrc522/
 import db_interface
 import improved_lcd
 import time
-import keyboard
+import keyboard # https://pypi.org/project/keyboard/
 
+# timing constants
 LASER_OFF_POLLING_RATE_SECONDS = 0.5
 LASER_ON_POLLING_RATE_SECONDS  = 1
 LASER_ON_GRACE_PERIOD_SECONDS  = 20
 
+# pin number constants
 LASER_RELAY_PIN_NUMBER = 8
-
 DONE_BUTTON_PIN_NUMBER = 10
-
 RED_LED_PIN_NUMBER = 32
 GREEN_LED_PIN_NUMBER = 33
 BLUE_LED_PIN_NUMBER = 35
 
-# ---------- keyboard handling stuff ---------
-
-shift_pressed = False
-accepting_keyboard_input = False
-
-def activate_keyboard_and_get_name(lcd):
-  global name_from_keyboard, keyboard_done, accepting_keyboard_input
+def GPIO_setup():
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setwarnings(False)
+  GPIO.setup(LASER_RELAY_PIN_NUMBER, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(DONE_BUTTON_PIN_NUMBER, GPIO.IN, pull_up_down=GPIO.PUD_UP) # enable Pi's built-in pull-up resistor for this pin
   
-  name_from_keyboard = ""
-  keyboard_done = False
-  accepting_keyboard_input = True
+  GPIO.setup(RED_LED_PIN_NUMBER, GPIO.OUT);
+  GPIO.setup(GREEN_LED_PIN_NUMBER, GPIO.OUT);
+  GPIO.setup(BLUE_LED_PIN_NUMBER, GPIO.OUT);
   
-  # allow last message to stay for a little while
-  # TODO: move instructions to enter name into here?
-  #  and maybe enable the keyboard before displaying instructions,
-  #  to capture any keys pressed as instructions are being displayed
-  #  if the user knows what to do already
-  time.sleep(2)
+  red = GPIO.PWM(RED_LED_PIN_NUMBER, 2);
+  green = GPIO.PWM(GREEN_LED_PIN_NUMBER, 2);
+  blue = GPIO.PWM(BLUE_LED_PIN_NUMBER, 2);
   
-  lcd.lcd_clear()
-  
-  while not keyboard_done:
-    #print(name_from_keyboard)
-    lcd.display_string(name_from_keyboard, 1)
-    time.sleep(0.25)
-  
-  accepting_keyboard_input = False
-  keyboard_done = False
-  
-  return name_from_keyboard
-
-def process_key_press(event):
-  global name_from_keyboard, keyboard_done, shift_pressed, accepting_keyboard_input
-  
-  if event.name == 'shift':
-    shift_pressed = True
-  
-  if not accepting_keyboard_input:
-    return
-  
-  if event.name == 'enter':
-    keyboard_done = True
-    return
-  
-  if event.name == 'backspace':
-    name_from_keyboard = name_from_keyboard[:-1]
-  
-  elif event.name == 'delete':
-    name_from_keyboard = ''
-  
-  elif event.name == 'space':
-    name_from_keyboard += ' '
-  
-  elif len(event.name) == 1:
-    if shift_pressed:
-      name_from_keyboard += event.name.upper()
-    else:
-      name_from_keyboard += event.name
-
-def process_shift_release(event):
-  global shift_pressed
-  
-  shift_pressed = False
-
-# ---------- end keyboard handling stuff -----
+  green.start(0)
+  red.start(0)
+  blue.start(0)
 
 # standardize checking done button
 #  since the button is connected between the input pin and ground
@@ -94,19 +46,11 @@ def is_done_button_pressed():
 
 def main():
   # -- keyboard setup --
-  
   keyboard.on_press(process_key_press)
   keyboard.on_release_key('shift', process_shift_release)
   
   # -- GPIO setup --
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setwarnings(False)
-  GPIO.setup(LASER_RELAY_PIN_NUMBER, GPIO.OUT, initial=GPIO.LOW)
-  GPIO.setup(DONE_BUTTON_PIN_NUMBER, GPIO.IN, pull_up_down=GPIO.PUD_UP) # enable Pi's built-in pull-up resistor for this pin
-  
-  GPIO.setup(RED_LED_PIN_NUMBER, GPIO.OUT); red = GPIO.PWM(RED_LED_PIN_NUMBER, 2); red.start(0)
-  GPIO.setup(GREEN_LED_PIN_NUMBER, GPIO.OUT); green = GPIO.PWM(GREEN_LED_PIN_NUMBER, 2); green.start(0)
-  GPIO.setup(BLUE_LED_PIN_NUMBER, GPIO.OUT); blue = GPIO.PWM(BLUE_LED_PIN_NUMBER, 2); blue.start(0)
+  GPIO_setup()
   
   # -- rfid setup --
   reader = SimpleMFRC522()
@@ -118,8 +62,8 @@ def main():
   
   # -- LCD setup --
   lcd = improved_lcd.lcd()
-  lcd.setup()
   
+  # status variable
   laser_just_finished_normally = False
   
   try:
@@ -129,11 +73,10 @@ def main():
       # if a user just finished using the laser cutter ...
       if laser_just_finished_normally:
         time.sleep(2)
-        # ... wait for the user to remove their card
-        lcd.display_string("please remove", 1)
-        lcd.display_string("your RamCard", 2)
-        time.sleep(5)
+        # ... wait for the user to remove their card ...
+        lcd.display_list_of_strings(["please remove", "your RamCard"], sleep_time=5)
         
+        # ... and then go back to the top of this while loop
         laser_just_finished_normally = False
         continue
       
@@ -143,12 +86,10 @@ def main():
       if not uid:
         # ... display the idle message, wait,
         #  and then go back to the top of this while loop
-        lcd.display_string("scan your", 1)
-        lcd.display_string("RamCard to use", 2)
         blue.ChangeDutyCycle(100)
         red.ChangeDutyCycle(0)
         green.ChangeDutyCycle(0)
-        time.sleep(LASER_OFF_POLLING_RATE_SECONDS)
+        lcd.display_list_of_strings(["scan your", "RamCard to use"], sleep_time=LASER_OFF_POLLING_RATE_SECONDS)
         continue
       
       row, duplicate = db.get_row_from_uid(uid)
@@ -164,19 +105,13 @@ def main():
           red.ChangeDutyCycle(100)
           blue.ChangeDutyCycle(100)
           green.ChangeDutyCycle(0)
-          lcd.display_string("adding a user!", 1)
-          lcd.display_string("scan new RamCard", 2)
-          time.sleep(5)
+          lcd.display_list_of_strings(["adding a user!", "scan new RamCard"], sleep_time=5)
+          
           uid_to_add = reader.read_id_no_block()
           if not uid_to_add: uid_to_add = reader.read_id_no_block()
           
           if not uid_to_add:
-            lcd.display_string("no card detected", 1)
-            lcd.display_string("admin, re-scan", 2)
-            time.sleep(2)
-            lcd.display_string("admin, re-scan", 1)
-            lcd.display_string("your RamCard", 2)
-            time.sleep(2)
+            lcd.display_list_of_strings(["no card detected", "admin, re-scan", "your RamCard"])
             continue
           
           data, duplicate = db.get_row_from_uid(uid_to_add)
@@ -193,25 +128,18 @@ def main():
             lcd.display_string("will be updated", 2)
             time.sleep(2)
           
-          #  prompt the user to enter their name with the keyboard
-          lcd.display_string("enter your name", 1)
-          lcd.display_string("on the keyboard", 2)
-          time.sleep(2)
-          lcd.display_string("press enter key", 1)
-          lcd.display_string("when done", 2)
-          
+          # user types in their name on the keyboard
           name_to_add = activate_keyboard_and_get_name(lcd)
           
-          #  and add them to the database as a user
-          #print("would have added a user with name %s, uid %d" % (name_to_add, uid_to_add))
+          # add them to the database as a user
           db.add_user(uid_to_add, name_to_add)
           
-          lcd.display_list_of_strings(["added user", name_to_add[:16], "with uid", hex(uid_to_add)])
+          lcd.display_list_of_strings(["added user", name_to_add, "with uid", hex(uid_to_add)], display_last_16=False)
           
           continue
         
         elif row:
-          lcd.display_string(db._get_name(row)[:16], 1)
+          lcd.display_string(db._get_name(row), 1, display_last_16=False)
           
         else:
           lcd.display_string("card uid:", 1)
@@ -288,16 +216,24 @@ def main():
         
         # if a card is present, check it against the database
         if uid:
-          row, duplicate = db.get_row_from_uid(uid) # TODO check if uid == current_user_uid first?
+          
+          if uid == current_user_uid:
+            times_card_missing = 0
+            green.ChangeDutyCycle(100)
+            red.ChangeDutyCycle(0)
+            lcd.display_string(name, 1)
+            lcd.display_uid_authorized()
+            continue
+          
+          row, duplicate = db.get_row_from_uid(uid)
           
           if row:
             name = db._get_name(row)
             lcd.display_string(name, 1)
             
-            # if the uid has not changed since the last check OR
-            #  the new uid is authorized,
-            #  go back to the top of this inner while loop
-            if uid == current_user_uid or db._check_uid(row):
+            # if the new uid is authorized,
+            #  update LED and LCD and go back to the top of this inner while loop
+            if db._check_uid(row):
               current_user_uid = uid
               times_card_missing = 0
               green.ChangeDutyCycle(100)
@@ -320,17 +256,14 @@ def main():
         #  so increment the number of times a check has not detected a valid card
         times_card_missing += 1
         
-        if display_card_missing:
-          red.ChangeDutyCycle(50) # blink at 2 Hz
-          green.ChangeDutyCycle(0)
-        
         # alert the user that they need to return their card to the reader
         #  and update how much time they have left to do so
-        if times_card_missing > 0:
-          if display_card_missing: lcd.display_string("card missing!", 1)
-          time_str = "%d sec to return" % ((max_times_card_missing - times_card_missing) * LASER_ON_POLLING_RATE_SECONDS)
-          lcd.display_string(time_str, 2)
-          continue
+        if display_card_missing:
+          lcd.display_string("card missing!", 1)
+          red.ChangeDutyCycle(50) # blink at 2 Hz
+          green.ChangeDutyCycle(0)
+        time_str = "%d sec to return" % ((max_times_card_missing - times_card_missing) * LASER_ON_POLLING_RATE_SECONDS)
+        lcd.display_string(time_str, 2)
   
   # if this program errors out,
   #  "turn off" the lcd and close the connection to the database
@@ -340,6 +273,65 @@ def main():
     lcd.backlight(0)
     db.close()
     # TODO print error for debugging purposes?
+
+# ---------- keyboard handling stuff ---------
+
+shift_pressed = False
+accepting_keyboard_input = False
+
+def activate_keyboard_and_get_name(lcd):
+  global name_from_keyboard, keyboard_done, accepting_keyboard_input
+  
+  name_from_keyboard = ""
+  keyboard_done = False
+  accepting_keyboard_input = True
+  
+  #  prompt the user to enter their name with the keyboard
+  lcd.display_list_of_strings(["enter your name", "on the keyboard", "press enter key", "when done"])
+  lcd.lcd_clear()
+  
+  while not keyboard_done:
+    lcd.display_string(name_from_keyboard, 1)
+    time.sleep(0.25)
+  
+  accepting_keyboard_input = False
+  
+  return name_from_keyboard
+
+def process_key_press(event):
+  global name_from_keyboard, keyboard_done, shift_pressed, accepting_keyboard_input
+  
+  if event.name == 'shift':
+    shift_pressed = True
+  
+  if not accepting_keyboard_input:
+    return
+  
+  if event.name == 'enter':
+    keyboard_done = True
+    return
+  
+  if event.name == 'backspace':
+    name_from_keyboard = name_from_keyboard[:-1]
+  
+  elif event.name == 'delete':
+    name_from_keyboard = ''
+  
+  elif event.name == 'space':
+    name_from_keyboard += ' '
+  
+  elif len(event.name) == 1:
+    if shift_pressed:
+      name_from_keyboard += event.name.upper()
+    else:
+      name_from_keyboard += event.name
+
+def process_shift_release(event):
+  global shift_pressed
+  
+  shift_pressed = False
+
+# ---------- end keyboard handling stuff -----
 
 if __name__ == "__main__":
   main()
