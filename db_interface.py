ca58c91731ce9cf210e1e0fac9edd20b2e3181ce
drohrbaugh9 <david.rohrbaugh7@gmail.com>
2023-11-24 16:50:44 -0700

    v1.4

--- db_interface.py
+++ db_interface.py
@@ -1,6 +1,36 @@
 import sqlite3
 import datetime
 
+# TODO extend dict instead of just having a dict?
+#  could be useful when sqlite requires a dict or dict subclass
+class user_entry:
+  
+  def __init__(self, uid: int, name: str, is_admin: int, expiration_date, duplicate: bool):
+    self.data = {'ramcard_uid': uid, 'fullname': name, 'is_admin': is_admin, 'expiration_date': expiration_date, 'duplicate': duplicate}
+  
+  def get_uid(self):
+    return self.data['ramcard_uid']
+  
+  def get_name(self):
+    return self.data['fullname']
+  
+  def is_admin(self):
+    if self.data['is_admin'] == 1:
+      return True
+    
+    return False
+  
+  def is_expired(self):
+    now = int(datetime.datetime.today().timestamp())
+    expiration = self.data['expiration_date']
+    
+    if now > expiration: return True
+    
+    return False
+  
+  def has_duplicate(self):
+    return self.data['duplicate']
+
 class db_interface:
 
   USER_ADD_ACTION = "ADD"
@@ -52,36 +82,35 @@ class db_interface:
     res = self._db_cursor.execute("SELECT * FROM users WHERE ramcard_uid = ?", [uid])
     
     row = res.fetchone()
-    duplicate = res.fetchone()
     
+    if not row:
+      return None
+    
+    duplicate = res.fetchone()
     found_duplicate = False
     if duplicate: found_duplicate = True
     
-    return row, found_duplicate
+    return user_entry(row[0], row[1], row[2], row[3], found_duplicate)
   
   def check_uid(self, uid: int):
-    data, duplicate = self.get_row_from_uid(uid)
+    data = self.get_row_from_uid(uid)
     
     return self._check_uid(data)
   
-  def _check_uid(self, row: tuple):
+  def _check_uid(self, row):
     
     if not row:
       return False
     
-    if self._is_admin(row):
-      self.log_unlock(self._get_uid(row))
-      return True
-    
-    if not self._is_expired(row):
-      self.log_unlock(self._get_uid(row))
+    if row.is_admin() or not row.is_expired():
+      self.log_unlock(row.get_uid())
       return True
     
     return False
   
   # only remove expired users, leave expired admins
   def remove_expired_users(self):
-    res = self._db_cursor.execute("DELETE FROM users WHERE is_admin = 0 AND expiration_date < ?", [int(datetime.datetime.today().timestamp())])
+    res = self._db_cursor.execute("DELETE FROM users WHERE is_admin != 1 AND expiration_date < ?", [int(datetime.datetime.today().timestamp())])
     
     self._db_cursor.execute("INSERT INTO users_log VALUES (?, ?, ?)", [int(datetime.datetime.today().timestamp()), self.USER_REMOVEEXPIRED_ACTION, res.rowcount])
     
@@ -105,41 +134,26 @@ class db_interface:
   ##### GET METHODS #####
   
   def get_name(self, uid: int):
-    data, duplicate = self.get_row_from_uid(uid)
+    data = self.get_row_from_uid(uid)
     
-    return self._get_name(data)
-  
-  def _get_name(self, row: tuple):
-    return row[1]
-  
-  def _get_uid(self, row: tuple):
-    return row[0]
+    return data.get_name()
   
   ##### IS METHODS #####
   
   def is_admin(self, uid: int):
-    data, duplicate = self.get_row_from_uid(uid)
+    data = self.get_row_from_uid(uid)
     
-    return self._is_admin(data)
+    return data.is_admin()
   
   def is_expired(self, uid: int):
-    data, duplicate = self.get_row_from_uid(uid)
+    data = self.get_row_from_uid(uid)
     
-    return self._is_expired(data)
-  
-  def _is_admin(self, row: tuple):
-    if row[2] == 1: return True
-    
-    return False
-  
-  def _is_expired(self, row: tuple):
-    now = int(datetime.datetime.today().timestamp())
-    expiration = row[3]
-    
-    if now > expiration: return True
-    
-    return False
+    return data.is_expired()
 
+# TODO change this to be something like
+#  - end of the current semester
+#  - end or current year
+#  - one of the above or 3 months, whichever is longer
 def calculate_expiration_date_timestamp():
   now = datetime.datetime.today()
   six_months = datetime.timedelta(days = 180)
